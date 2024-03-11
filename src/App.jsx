3cdeff277949080bbbe9f3a2cde30c02eb4726bd
hannahf86 <hannahfeehan.design@gmail.com>
2024-03-11 12:09:13 +0000

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,30 @@ import About from "./scenes/about";
 import Faq from "./scenes/help/Faq";
 import Contact from "./scenes/help/contact";
 import NotFound from "./scenes/notFound";
+import Careers from "./scenes/careers/Careers";
 
 // layouts
 import RootLayout from "./layouts/RootLayout";
 import HelpLayout from "./layouts/HelpLayout";
+import CareersLayout from "./layouts/CareersLayout";
+
+// loaders
+import { careersLoader } from "./scenes/careers/Careers";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
+      {/* PAGES */}
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="help" element={<HelpLayout />}>
+        {/* NESTED PAGES */}
         <Route path="faq" element={<Faq />} />
         <Route path="contact" element={<Contact />} />
+      </Route>
+
+      <Route path="careers" element={<CareersLayout />}>
+        <Route index element={<Careers />} loader={careersLoader} />
       </Route>
 
       {/* ERROR HANDLING - CATCH ALL */}
---
+++ src/scenes/careers/Careers.jsx
@@ -0,0 +1,26 @@
+import { Link, useLoaderData } from "react-router-dom";
+
+export default function Careers() {
+  const careers = useLoaderData();
+
+  return (
+    <div className="careers">
+      {careers.map((career) => (
+        <Link to="/" key={career.id}>
+          <p>{career.title}</p>
+          <p>Based in {career.location}</p>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+// data loader
+export const careersLoader = async () => {
+  {
+    /* API ENDPOINT */
+  }
+  const res = await fetch("http://localhost:3000/careers");
+
+  return res.json();
+};
