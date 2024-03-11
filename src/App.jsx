41cbde8ad3df305cbc9eeeb12a1487a4ec6c4979
hannahf86 <hannahfeehan.design@gmail.com>
2024-03-11 11:53:17 +0000

    help nav added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-// RRD
 import {
   createBrowserRouter,
-  Route,
   createRoutesFromElements,
+  Route,
   RouterProvider,
 } from "react-router-dom";
 
-// SCENES
+// pages
 import Home from "./scenes/home";
 import About from "./scenes/about";
+import Faq from "./scenes/help/Faq";
+import Contact from "./scenes/help/contact";
+
+// layouts
 import RootLayout from "./layouts/RootLayout";
+import HelpLayout from "./layouts/HelpLayout";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<RootLayout />}>
-      {/* OUTLET */}
+    <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" element={<Faq />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
     </Route>
   )
 );
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -8,10 +8,9 @@ export default function RootLayout() {
           <h1>Jobarouter</h1>
           <NavLink to="/">Home</NavLink>
           <NavLink to="about">About</NavLink>
+          <NavLink to="help">Help</NavLink>
         </nav>
       </header>
-
-      {/* MAIN CONTENT GOES IN OUTLET */}
       <main>
         <Outlet />
       </main>
